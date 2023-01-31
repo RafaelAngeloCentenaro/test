08e732f00023076b853f217eeceaf62f4f153b4d
RafaelAngeloCentenaro <105504416+RafaelAngeloCentenaro@users.noreply.github.com>
2023-01-31 19:07:26 -0300

    Add files via upload

---
+++ screens/Profile.js
@@ -0,0 +1,11 @@
+import React,{Components} from "react";
+import {View,Text} from "react-native";
+export default class Profile extends Components{
+    render(){
+        return(
+            <View>
+            <Text>tela de perfil</Text>
+            </View>
+        )
+    }
+}
